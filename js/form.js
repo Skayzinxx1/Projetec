505d5879e050592452692749226348587e099f53
Skayzinxx1 <matheusgeo50@gmail.com>
2026-01-16 15:40:00 -0300

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -1,37 +1,37 @@
 function enviarEmail() {
- 
-    const nomeInput = document.getElementById('nome');
-    const emailInput = document.getElementById('email');
-    const mensagemInput = document.getElementById('mensagem');
 
-  
-    if (!nomeInput.value || !mensagemInput.value) {
-        alert("Por favor, preencha o seu nome e a descrição do serviço.");
+    const nome = document.getElementById("nome").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const mensagem = document.getElementById("mensagem").value.trim();
+
+    if (!nome || !email || !mensagem) {
+        alert("Preencha todos os campos.");
         return;
     }
 
-
-    const meuEmail = "matheusgeo56@mail.com"; 
+    const para = "vendas@projetecbrasil.com";
     const assunto = encodeURIComponent("Solicitação de Serviço - Projetec");
+
     const corpo = encodeURIComponent(
-        "Nome: " + nomeInput.value + "\n" +
-        "E-mail do Cliente: " + emailInput.value + "\n\n" +
-        "Descrição: " + mensagemInput.value
+        `Nome: ${nome}\nE-mail: ${email}\n\nMensagem:\n${mensagem}`
     );
 
+    const gmailURL =
+        `https://mail.google.com/mail/?view=cm&fs=1&to=${para}&su=${assunto}&body=${corpo}`;
 
-    window.location.href = "mailto:" + meuEmail + "?subject=" + assunto + "&body=" + corpo;
+    window.open(gmailURL, "_blank");
 }
+
 function enviarWhats() {
     const nome = document.getElementById('nome').value;
     const mensagem = document.getElementById('mensagem').value;
-    const telefone = "553438316333";
+    const telefone = "5534999844058";
 
     if (!nome || !mensagem) {
         alert("Por favor, preencha o nome e a descrição.");
         return;
     }
 
-    const texto = encodeURIComponent(`Olá, meu nome é ${nome}. Preciso de: ${mensagem}`);
+    const texto = encodeURIComponent(`Olá, meu nome é ${nome}. Solícito: ${mensagem}`);
     window.open(`https://wa.me/${telefone}?text=${texto}`, '_blank');
 }
